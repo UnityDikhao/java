6ed5327db9b43ada915e19b46f76a772202e0250
UnityDikhao <166284991+UnityDikhao@users.noreply.github.com>
2024-04-17 20:36:06 +0530

    Update Scroll.java

--- s30/Scroll.java
+++ s30/Scroll.java
@@ -7,25 +7,24 @@ public class Scroll {
         ResultSet resultSet = null;
 
         try {
-            // Establish connection to the database
+            
             connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/", "", "");
 
-            // Create a statement with scrollable ResultSet
+            
             statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
 
-            // Execute a query to fetch data from the Teacher table
+         
             resultSet = statement.executeQuery("SELECT * FROM Teacher");
 
-            // Move the cursor to the last row of the ResultSet
+            
             resultSet.last();
 
-            // Get the total number of rows in the ResultSet
+            
             int totalRows = resultSet.getRow();
 
-            // Move the cursor to the first row of the ResultSet
             resultSet.beforeFirst();
 
-            // Iterate over the ResultSet and display the data
+          
             System.out.println("Teacher Table:");
             System.out.println("TNo\tTName\tSalary");
             while (resultSet.next()) {
@@ -35,12 +34,12 @@ public class Scroll {
                 System.out.println(tid + "\t" + tname + "\t" + salary);
             }
 
-            // Display the total number of rows in the ResultSet
+        
             System.out.println("Total Rows: " + totalRows);
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
-            // Close the resources
+         
             try {
                 if (resultSet != null) resultSet.close();
                 if (statement != null) statement.close();
